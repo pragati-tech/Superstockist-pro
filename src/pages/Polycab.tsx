78afc8d14a7af09248cc22d14e7a8869469f583a
PRAGATI JAIN <jpragati05@gmail.com>
2025-08-14 14:55:39 +0530

    Update Polycab.tsx

--- src/pages/Polycab.tsx
+++ src/pages/Polycab.tsx
@@ -50,7 +50,7 @@ const PolycabPage = () => {
   </div>
 </a>
           {/* Card 2 - Duplicate for now */}
-          <a target="_blank" href={Catalogue2img}>
+          <a target="_blank" href={Catalogue2}>
    <div className="group relative w-[400px] h-[350px] rounded-2xl bg-white/70 backdrop-blur-lg shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-500 border border-gray-200">
     <div
         className="w-[400px] h-[350px] bg-center bg-cover m-auto transition-transform duration-500 group-hover:scale-110"
