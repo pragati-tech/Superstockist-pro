fba8a7132a62f3d8235cd3c6abb54cc7c4095ad6
PRAGATI JAIN <jpragati05@gmail.com>
2025-08-27 22:46:48 +0530

    Update DirectorsPage.tsx

--- src/pages/DirectorsPage.tsx
+++ src/pages/DirectorsPage.tsx
@@ -7,13 +7,18 @@ const DirectorPage = () => {
       name: "Mr. Anuj Khandelwal",
       photo: "/images/director1.jpg",
       message:
-        "As a director, I believe in driving innovation while keeping our core values intact. Our mission is to create impactful solutions for the future.",
+        "As a Director at Krishna Tradlinks Pvt. Ltd. and Khandelwal Enterprises. With over 40 years of extensive experience in the distribution and retail across diverse industries, his expertise spans Watches, Jewellery, Lighting, Appliances, and FMCG.
+        Since 1985, he has been the distributor for Nestle in Gwalior. He also has led the expansion of Titan, Sonata and Fastrack in the Gwalior region through distribution networks and exclusive retail stores. 
+        In the lighting and appliances segment, Mr. Khandelwal oversaw the distribution of Philips from 1992 to 2010, expanded operations with Havells from 2010, and, since 2024, holds the distribution for Polycab.
+        His commitment to growth, innovation, and delivering partner- and customer-centric solutions has fostered enduring, value-driven relationships across the markets.",
     },
     {
       name: "Ms. Seema Khandelwal",
       photo: "/images/director2.jpg",
       message:
-        "Leadership is about empowering teams to achieve their full potential. Together, we can build a better and more sustainable tomorrow.",
+        "She is the Director at Krishna Tradlinks Pvt. Ltd. and Khandelwal Enterprises. With over 17 years of experience in the retail watch business, she has played a key role in bringing leading watch brands to Gwalior.
+        In 2008, she led the launch of Gwalior’s first exclusive World of Titan store, introducing a modern watch retail experience. Ten years later, in 2018, she played a vital role in the opening of the city's first Helios store, offering customers an extensive collection of luxury multi-brand watches. Building on this success, she opened the Fastrack store in 2019, catering specifically to the youth segment.
+        Her emphasis on innovation, customer satisfaction, and brand growth has helped build strong and lasting relationships with both customers and partners.",
     },
   ];
 
